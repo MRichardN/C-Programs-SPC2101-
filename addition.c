79b8c2c731cc9bdda0d40f827c29b54880640188
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2025-09-19 12:34:46 +0300

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,19 @@
+/*
+Auhtor: Alex
+Reg No:
+Date: 19th Sept 2025
+Description: Program to add two numbers
+*/
+//pre-processor directive
+#include<stdio.h> //scanf(), printf()
+
+int main(){
+	//variable declaration & initialization
+	int num1 = 10;
+	int num2 = 20;
+	int sum;
+	sum = num1 + num2;
+	printf("The sum is %d ", sum);
+	
+	return 0;
+}
